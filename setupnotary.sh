26eb797ae8b8a6db75992dbafd11eba864580fbd
rajumariappan <rajumail@gmail.com>
2016-01-19 14:44:42 -0500

    Update setupnotary.sh

--- setupnotary.sh
+++ setupnotary.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-
+!/bin/bash
+rm -rf botocore  govern8r  govern8rClient  govern8rLib govern8rService  python-bitcoinlib
 # one time command to update python in  MAC env.
 #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #brew install python
@@ -8,58 +8,61 @@
 
 # start to download all the repos
 git clone https://github.com/boto/botocore.git
-git clone https://github.com/rajumariappan/govern8r.git
-git clone https://github.com/rajumariappan/govern8rClient.git
-git clone https://github.com/rajumariappan/govern8rService.git
-git clone https://github.com/rajumariappan/govern8rLib.git
+git clone https://github.com/cypherhat/govern8r.git
+git clone https://github.com/cypherhat/govern8rClient.git
+git clone https://github.com/cypherhat/govern8rService.git
+git clone https://github.com/cypherhat/govern8rLib.git
 git clone https://github.com/cypherhat/python-bitcoinlib.git
 
-
-virtualenv -p /usr/bin/python govern8rClient
-virtualenv -p /usr/bin/python govern8rService
-
-source govern8rService/bin/activate
-sudo pip install flask
-sudo pip install flask-api
-sudo pip install pybitid
-sudo pip install blockcypher
-sudo pip install certifi
-sudo pip install configparser
-sudo pip install ecdsa
-sudo pip install pycrypto
-sudo pip install simple-crypt
-
+pwd
+virtualenv -p /usr/bin/python vClient
+virtualenv -p /usr/bin/python vService
+pwd
+source vService/bin/activate
+pip install flask
+pip install flask-api
+pip install pybitid
+pip install blockcypher
+pip install certifi
+pip install configparser
+pip install ecdsa
+pip install pycrypto
+pip install simple-crypt
+pip install pyOpenSSL
+pwd
 cd botocore
-sudo python setup.py install
+python setup.py install
 cd ..
 
-sudo pip install boto3
-sudo pip install awscli
+pip install boto3
+pip install awscli
 
+pwd
 cd python-bitcoinlib
-sudo python setup.py install
-cd ..     
-         
-cd govern8r/govern8rLib
-sudo python setup.py install 
-cd ../..     
+python setup.py install
+cd ..
+pwd
+cd govern8rLib
+python setup.py install
+cd ..
 
 deactivate
-    
-    
-source govern8rClient/bin/activate
-sudo pip install requests
-sudo pip install ecdsa
-sudo pip install pycrypto
-sudo pip install configparser
-sudo pip install simple-crypt
-    
+
+pwd
+source vClient/bin/activate
+pip install requests
+pip install ecdsa
+pip install pycrypto
+pip install configparser
+pip install simple-crypt
+
+cd govern8rLib
+python setup.py install
+cd ..
+pwd
 cd python-bitcoinlib
 sudo python setup.py install
 cd ..
-
-cd govern8r/govern8rLib
-sudo python setup.py install
-cd ../..
-
+pwd
 deactivate
+
