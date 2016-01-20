cf3f3341a9cb06854916a5bac816fea43f21cffd
rajumariappan <rajumail@gmail.com>
2016-01-20 11:51:11 -0500

    requirement file based setup script.

--- setupnotary.sh
+++ setupnotary.sh
@@ -1,5 +1,5 @@
-!/bin/bash
-rm -rf botocore  govern8r  govern8rClient  govern8rLib govern8rService  python-bitcoinlib
+#!/bin/bash
+rm -rf botocore   govern8r  govern8rClient govern8rLib  govern8rService python-bitcoinlib
 # one time command to update python in  MAC env.
 #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #brew install python
@@ -14,55 +14,30 @@ git clone https://github.com/cypherhat/govern8rService.git
 git clone https://github.com/cypherhat/govern8rLib.git
 git clone https://github.com/cypherhat/python-bitcoinlib.git
 
+#create virtual envs.
 pwd
 virtualenv -p /usr/bin/python vClient
 virtualenv -p /usr/bin/python vService
-pwd
 source vService/bin/activate
-pip install flask
-pip install flask-api
-pip install pybitid
-pip install blockcypher
-pip install certifi
-pip install configparser
-pip install ecdsa
-pip install pycrypto
-pip install simple-crypt
-pip install pyOpenSSL
+
 pwd
 cd botocore
 python setup.py install
 cd ..
-
-pip install boto3
-pip install awscli
-
 pwd
-cd python-bitcoinlib
-python setup.py install
-cd ..
-pwd
-cd govern8rLib
-python setup.py install
-cd ..
 
+pip install -r govern8rService/requirements.txt
+pwd
 deactivate
 
 pwd
+
 source vClient/bin/activate
-pip install requests
-pip install ecdsa
-pip install pycrypto
-pip install configparser
-pip install simple-crypt
-
-cd govern8rLib
+cd botocore
 python setup.py install
 cd ..
-pwd
-cd python-bitcoinlib
-sudo python setup.py install
-cd ..
+pip install -r govern8rClient/requirements.txt
 pwd
 deactivate
+
 
